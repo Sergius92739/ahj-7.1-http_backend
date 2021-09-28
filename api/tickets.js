c249d65a4e24f0b0e32389600ce2ab0c67c12795
Sergius92739 <sergem279@gmail.com>
2021-09-28 11:10:17 +0300

    fixed

--- api/tickets.js
+++ api/tickets.js
@@ -1,25 +1,3 @@
-const tickets = [
-    {
-      id: 'c20fa24d-bea9-4faf-a326-a0cb95cd9141',
-      name: 'Поменять краску в принтере',
-      description: 'Принтер HP LJ1210, катриджи на складе',
-      status: false,
-      created: '24.09.2021, 20:37:43'
-    },
-    {
-      id: '8f8133a7-3bd3-48b3-869f-d618d9239f59',
-      name: 'Переустановить Windows, ПК-Hall24',
-      description: 'После переустановки установить WebStorm',
-      status: false,
-      created: '24.09.2021, 20:37:43'
-    },
-    {
-      id: 'd6040fd0-aded-4a43-a53c-93714e0231a5',
-      name: 'Установить обновление KB-XXX',
-      description: 'Вышло критическое обновление для Windows, нужно поставить обновления в следующем приоритете: 1. Сервера (не забыть сделать бэкап!) 2. Рабочие станции.',
-      status: false,
-      created: '24.09.2021, 20:37:43'
-    }
-  ]
+const tickets = []
 
 module.exports = tickets;
